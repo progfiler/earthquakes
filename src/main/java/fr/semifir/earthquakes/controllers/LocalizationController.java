c3a23ddace527fd526e10b5f1632c42a9383cd3b
antoine fissot <antoine@semifir.com>
2021-12-09 12:03:03 +0100

    feat: create localization controller

---
+++ src/main/java/fr/semifir/earthquakes/controllers/LocalizationController.java
@@ -0,0 +1,56 @@
+package fr.semifir.earthquakes.controllers;
+
+import fr.semifir.earthquakes.dtos.localization.LocalizationDTO;
+import fr.semifir.earthquakes.services.LocalizationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("localizations")
+public class LocalizationController {
+
+    private LocalizationService service;
+
+    public LocalizationController(LocalizationService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<LocalizationDTO> findAll() {
+        return this.service.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<LocalizationDTO> find(
+            @PathVariable String id;
+    ) {
+         LocalizationDTO localizationDTO = this.service.find(id);
+         if (localizationDTO == null) {
+             return ResponseEntity.notFound().build();
+         }
+         return ResponseEntity.ok(localizationDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<LocalizationDTO> save(
+            @RequestBody LocalizationDTO localizationDTO) {
+        LocalizationDTO localizationDTOSaved  = this.service.save(localizationDTO);
+        return ResponseEntity.ok(localizationDTO);
+    }
+
+    @PutMapping
+    public ResponseEntity<LocalizationDTO> update(
+            @RequestBody LocalizationDTO localizationDTO) {
+        LocalizationDTO localizationDTOSUpdated  = this.service.update(localizationDTO);
+        return ResponseEntity.ok(localizationDTO);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> delete(
+            @RequestBody LocalizationDTO localizationDTO) {
+        this.service.delete(localizationDTO);
+        return ResponseEntity.ok(true);
+    }
+}
